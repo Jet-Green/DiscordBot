b7dba460b9775f5fcb2091eb82133b5e57ff0622
Jet-Green <grishadzyin@gmail.com>
2021-11-06 13:02:27 +0400

    user's roles to firebase and message on join

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const DiscordJS = require('discord.js')
 const WOKCommands = require('wokcommands')
 const path = require('path')
-// const db = require('./firebaseinit').admin.firestore()
+const db = require('./firebaseinit').admin.firestore()
 const { dirRoles, lvlRoles, lvlPostId, dirPostId } = require('./config')
 require('dotenv').config()
 const { Intents } = DiscordJS
@@ -13,6 +13,7 @@ const client = new DiscordJS.Client({
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MEMBERS
   ],
 })
 // bot init
@@ -70,19 +71,32 @@ client.on('guildMemberAdd', async (member) => {
   let id = user_temp.id
   // CLEAN THE OBJECT
   delete user_temp.id
+  delete user_temp.flags
 
   await db.collection('users').doc(id).set(user_temp).catch(error => console.error("Can't add new user", error))
   console.log('Doc have been added to collection: ', user_temp)
 
   channel.send(message)
+
+  var embed = new DiscordJS.MessageEmbed()
+    .setTitle(`Hi, ${member.displayName}`)
+    .setColor('#ff0051')
+    .setDescription('Welcome ......................................')
+    .setFooter(`ID: ${member.id} | ................`, '')
+  // .setImage('')
+  // .setThumbnail('')
+
+  member.createDM().then(DMchannel => {
+    member.send({ embeds: [embed] })
+  })
 })
 
 client.on('messageReactionAdd', async (reaction, user) => {
   let message = reaction.message
+
   if (message.channel.id == '905440828808384542') {
     let member = client.guilds.cache.get('903878049307697152').members.cache.get(user.id)
     let emoji = reaction.emoji.name
-    // console.log(member)
     if (message.id == lvlPostId) {
       for (em in lvlRoles) {
         if (emoji == em) {
@@ -104,22 +118,43 @@ client.on('messageReactionRemove', async (reaction, user) => {
   if (message.channel.id == '905440828808384542') {
     let member = client.guilds.cache.get('903878049307697152').members.cache.get(user.id)
     let emoji = reaction.emoji.name
-    // console.log(member)
     if (message.id == lvlPostId) {
-      console.log('lvl')
-      for (em in lvlRoles) {
-        if (emoji == em) {
-          member.roles.remove(lvlRoles[em])
-        }
-      }
+      console.log('lvl rm')
+      for (em in lvlRoles) { if (emoji == em) { member.roles.remove(lvlRoles[em]) } }
     } else {
-      for (em in dirRoles) {
-        if (emoji == em) {
-          member.roles.remove(dirRoles[em])
-        }
-      }
+      console.log('dir rm')
+      for (em in dirRoles) { if (emoji == em) { member.roles.remove(dirRoles[em]) } }
     }
   }
 })
+// @TODO: update nickname
+client.on('guildMemberUpdate', async (oldMember, newMember) => {
+  // If the role(s) are present on the old member object but no longer on the new one (i.e role(s) were removed)
+  const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+  if (removedRoles.size > 0) {
+    console.log(`The roles ${removedRoles.map(r => r.name)} were removed from ${oldMember.displayName}.`);
+  }
+
+  // If the role(s) are present on the new member object but are not on the old one (i.e role(s) were added)
+  const addedRoles = newMember.roles.cache.filter(role => !oldMember.roles.cache.has(role.id));
+  if (addedRoles.size > 0) {
+    console.log(`The roles ${addedRoles.map(r => r.name)} were added to ${oldMember.displayName}.`);
+  }
+  updateRolesInDB(newMember, newMember.user)
+});
+
+async function updateRolesInDB(member, user) {
+  roles = []
+  for (let role of member.roles.cache.values()) {
+    roles.push({
+      id: role.id,
+      name: role.name
+    })
+  }
+  console.log(roles)
+  await db.collection('users').doc(user.id)
+    .update({ "roles": roles })
+    .catch(error => console.error("Can't update user's role", error))
+}
 
 client.login(process.env.TOKEN)
